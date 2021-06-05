60b4a6db2c82a66ebd80be562fc55d14e3faffd0
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-06-05 19:31:19 -0300

    basic stetic

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,52 @@
 import React, { useState } from 'react'
 import './app.css'
+import { when } from "view-logics";
 
 export function App(): React.ReactElement {
 	const [url, setUrl] = useState("");
   const [response, setResponse] = useState("");
   const [HTTPMethod, setHTTPMethod] = useState("GET");
-  const getData = () => fetch(url, {method:HTTPMethod})
+  const [body, setBody] = useState(undefined);
+  const [headers, setHeaders] = useState(undefined);
+  const getData = () => fetch(url, {method:HTTPMethod, body: JSON.stringify(body)})
     .then(response => response.json())
     .then(data => setResponse(JSON.stringify(data)));
+  const input = "HTTPRequest";
+  const output = "textarea";
+  const showBody = true;
+  const showheaders = false;
 
-  return (
-    <>
-      <input type="text" onChange={({target})=>setUrl(target.value)} />
-          <select value={HTTPMethod} onChange={({target})=>setHTTPMethod(target.value)}>
-            <option value="GET">GET</option>
-            <option value="POST">POST</option>
-            <option value="DELETE">DELETE</option>
-            <option value="PUT">PUT</option>
-            <option value="PATCH">PATCH</option>
-          </select>
-      <button onClick={getData}>{"|>"}</button>
+  return when([
+    [input === "HTTPRequest",
+      <section>
+        <label>URL:
+          <input type="text" onChange={({target})=>setUrl(target.value)} />
+        </label>
+        <select value={HTTPMethod} onChange={({target})=>setHTTPMethod(target.value)}>
+          <option value="GET">GET</option>
+          <option value="POST">POST</option>
+          <option value="DELETE">DELETE</option>
+          <option value="PUT">PUT</option>
+          <option value="PATCH">PATCH</option>
+        </select>
+        <button onClick={getData}>{">"}</button>
+      </section>
+    ],
+    [showBody,
+      <label>Body:
+        <input type="text" onChange={({target})=>setBody(target.value)} />
+      </label>
+    ],
+    [showheaders,
+      <label>headers:
+        <input type="text" onChange={({target})=>setHeaders(target.value)} />
+      </label>
+    ],
+    [output === "textarea",
+    <section>
+      <label>Response:</label>
       <textarea value={response}/>
-    </>
-  );
+    </section>
+    ],
+  ])
 }
